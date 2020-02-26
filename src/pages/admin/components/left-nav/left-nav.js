183cfc8bb4183415882e2755baef7188afbe9af5
Mochen07 <mochen7777@gmail.com>
2020-02-26 22:51:40 +0800

    modify detail

--- src/pages/admin/components/left-nav/left-nav.js
+++ src/pages/admin/components/left-nav/left-nav.js
@@ -90,7 +90,28 @@ class LeftNav extends Component {
             //         menuList: result.data
             //     })
             // }
-            const menuList = {"status":0,"data":[{"id":1,"title":"首页","_key":"/home","icon":"home","parentID":0},{"id":2,"title":"个人信息","_key":"/user","icon":"user","parentID":0},{"id":3,"title":"菜单管理","_key":"/category","icon":"appstore","parentID":0},{"id":4,"title":"题目管理","_key":"/questions","icon":"container","parentID":0,"children":[{"id":5,"title":"Java","_key":"/question/java","icon":"android","parentID":4},{"id":6,"title":"Python","_key":"/question/python","icon":"bug","parentID":4},{"id":7,"title":"H5","_key":"/question/h5","icon":"html5","parentID":4}]},{"id":8,"title":"统计","_key":"/charts","icon":"area-chart","parentID":0,"children":[{"id":9,"title":"柱形图","_key":"/charts/bar","icon":"bar-chart","parentID":8},{"id":10,"title":"折线图","_key":"/charts/line","icon":"line-chart","parentID":8},{"id":11,"title":"饼图","_key":"/charts/pie","icon":"pie-chart","parentID":8}]}]}
+            const menuList = {
+                "status":0,
+                "data":[
+                    {"id":1,"title":"首页","_key":"/home","icon":"home","parentID":0},
+                    {"id":2,"title":"个人信息","_key":"/user","icon":"user","parentID":0},
+                    {"id":3,"title":"菜单管理","_key":"/category","icon":"appstore","parentID":0},
+                    {"id":4,"title":"题目管理","_key":"/questions","icon":"container","parentID":0,
+                        "children":[
+                            {"id":5,"title":"Java","_key":"/question/java","icon":"android","parentID":4},
+                            {"id":6,"title":"Python","_key":"/question/python","icon":"bug","parentID":4},
+                            {"id":7,"title":"H5","_key":"/question/h5","icon":"html5","parentID":4}
+                        ]
+                    },
+                    {"id":8,"title":"统计","_key":"/charts","icon":"area-chart","parentID":0,
+                        "children":[
+                            {"id":9,"title":"柱形图","_key":"/charts/bar","icon":"bar-chart","parentID":8},
+                            {"id":10,"title":"折线图","_key":"/charts/line","icon":"line-chart","parentID":8},
+                            {"id":11,"title":"饼图","_key":"/charts/pie","icon":"pie-chart","parentID":8}
+                        ]
+                    }
+                ]
+            }
             this.setState({
                 menuList: menuList.data
             })
